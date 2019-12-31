b6f868a6034ab314c2fbe29e8e0565c98063a0cc
Darran Kelinske <darran.kelinske@gmail.com>
2019-12-31 16:35:33 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/whereisdarran/com/flutter_web_google_maps/MainActivity.kt
@@ -0,0 +1,12 @@
+package whereisdarran.com.flutter_web_google_maps
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
